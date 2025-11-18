6635c2e13d7be7eaf8f5a8937905eb7a8700945a
wataruyagihashi <wataru_yagihashi@mail.toyota.co.jp>
2025-11-18 18:43:15 +0900

    デプロイのためserver.jsの内容を編集

--- server.js
+++ server.js
@@ -3,17 +3,17 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 
-//Knex ポスグレ接続
-const knex = require("knex")({
-  client: "pg",
-  connection: {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-  },
-});
+// //Knex ポスグレ接続
+// const knex = require("knex")({
+//   client: "pg",
+//   connection: {
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//   },
+// });
 
 const app = express();
 const PORT = process.env.PORT || 3000;
